f200550ac574f5d73184d4a7e4d43f23f3f4eee3
Juan Kevin Trujillo <me@juankevintrujillo.com>
2022-02-12 03:27:06 +0000

    Create Outlets in Interface Builder

---
+++ Today/ReminderListCell.swift
@@ -0,0 +1,14 @@
+//
+//  ReminderListCell.swift
+//  Today
+//
+//  Created by Juan Kevin Trujillo on 12/2/22.
+//
+
+import UIKit
+
+class ReminderListCell: UITableViewCell {
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var doneButton: UIButton!
+}
